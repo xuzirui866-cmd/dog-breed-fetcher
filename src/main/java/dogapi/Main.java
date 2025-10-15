c6855cbef87653874612784904d6b7246bfbc4b9
Zirui Xu <xuzirui866@gmail.com>
2025-10-15 17:28:57 -0400

    Task

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BreedFetcher.BreedNotFoundException {
         String breed = "hound";
         BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
         int result = getNumberOfSubBreeds(breed, breedFetcher);
@@ -18,14 +18,19 @@ public class Main {
     /**
      * Return the number of sub breeds that the given dog breed has according to the
      * provided fetcher.
-     * @param breed the name of the dog breed
+     *
+     * @param breed        the name of the dog breed
      * @param breedFetcher the breedFetcher to use
      * @return the number of sub breeds. Zero should be returned if there are no sub breeds
      * returned by the fetcher
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-        // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
-        // return statement included so that the starter code can compile and run.
-        return -1;
+        try {
+            List<String> subbreed = breedFetcher.getSubBreeds(breed);
+            return subbreed.size();
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            System.out.println("Breed not found: " + breed);
+            return 0;
+        }
     }
 }
